18683c2db3ebbe2f8b106162e71cf98ffa31e2b7
rodri <rodri@DESKTOP-HJ4KKVS>
2022-11-14 09:53:22 -0600

    Agregar archivos de proyecto.

---
+++ Unidad2/ImageBrushWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Unidad2
+{
+    /// <summary>
+    /// Lógica de interacción para ImageBrushWindow.xaml
+    /// </summary>
+    public partial class ImageBrushWindow : Window
+    {
+        public ImageBrushWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
