95c423dc7a3cfea5553057e10445c1ef5333df3d
Rodrigo Elias Castillo <201G0246@rcarbonifera.tecnm.mx>
2022-12-05 09:26:08 -0600

    Unidad 4

---
+++ Unidad4/Elipse.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Unidad4
+{
+    /// <summary>
+    /// Lógica de interacción para Elipse.xaml
+    /// </summary>
+    public partial class Elipse : Window
+    {
+        public Elipse()
+        {
+            InitializeComponent();
+        }
+
+        private void cnvElipses_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point posicion = e.GetPosition(cnvElipses);
+            Random r = new Random();
+            Ellipse elipse = new Ellipse
+            {
+                Width = 30,
+                Height = 30,
+                Fill = new SolidColorBrush(Color.FromRgb((byte)r.Next(0,256), (byte)r.Next(0,256), (byte)r.Next(0,256)))
+            };
+            cnvElipses.Children.Add(elipse);
+            Canvas.SetLeft(elipse, posicion.X);
+            Canvas.SetTop(elipse, posicion.Y);
+
+
+            DoubleAnimation animacion = new DoubleAnimation
+            {
+                To = cnvElipses.ActualHeight - elipse.Height,
+                BeginTime = TimeSpan.FromSeconds(0),
+                Duration = TimeSpan.FromSeconds(2),
+                EasingFunction=new BounceEase
+                {
+                    Bounces=5,
+                    EasingMode=EasingMode.EaseOut
+                }
+                
+            };
+
+            elipse.BeginAnimation(Canvas.TopProperty, animacion);
+        }
+    }
+}
